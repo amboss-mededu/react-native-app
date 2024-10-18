bfd9172e7647c984dfdce76e7ead6589969a01cc
Tahera Bodabhaiwala <tbo@amboss.com>
2024-10-18 17:00:42 +0200

    Conditionally render IOS Button

--- components/screens/Buttons.js
+++ components/screens/Buttons.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Alert, View} from 'react-native';
+import {Alert, View, Platform} from 'react-native';
 import { PrimaryButton } from '../native/PrimaryButton';
 import { PrimaryButton as DSPrimaryButton } from '../shared/PrimaryButton';
 import { Heading } from '../shared/Heading';
 
 export function Buttons() {
+  const nativeButtonElm = Platform.OS === 'ios' ?  <PrimaryButton title="Primary Button" onClick={() => Alert.alert(`clicked`)}/> :
+  <Text>Placeholder for Android Button</Text>
+
   return (
     <View style={{ flex: 1, flexDirection: 'column', padding: 20, gap: 20 }}>
-      <Heading level={2}>DS PrimaryButton from IOS</Heading>
+      <Heading level={2}>DS PrimaryButton from {Platform.OS} </Heading>
       <View style={{height: 48}}>
-        <PrimaryButton title="Primary Button" onClick={() => Alert.alert(`clicked`)}/>
+        {nativeButtonElm}
       </View>
       <Heading level={2}>React native Button styled as DS PrimaryButton</Heading>
       <View style={{height: 48, width: 200}}>
